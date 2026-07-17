c87bc751ab6e12157a8570bd3b90a43fed4259e0
Marcus Miller <millamarcom@gmail.com>
2026-07-17 18:47:11 -0400

    Commit inicial: Atraque Mejillones PWA

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'atraque-mejillones-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Estrategia simple: intenta la red primero (para tener datos frescos),
+  // y si falla (sin conexión), usa lo que haya en cache.
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
